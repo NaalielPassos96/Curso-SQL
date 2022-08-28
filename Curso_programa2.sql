994b4ef34e518d4891b23d77cb35328d34a0a553
Naaliel Passos <94538782+NaalielPassos96@users.noreply.github.com>
2022-08-27 23:09:57 -0300

    Add files via upload

---
+++ Curso_programa2.sql
@@ -0,0 +1,50 @@
+/** Comentário **/
+-- Comentário
+DROP DATABASE IF EXISTS aula_banco;
+
+CREATE DATABASE aula_banco;
+
+USE aula_banco;
+
+DROP TABLE IF EXISTS estado;
+
+CREATE TABLE estado(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+,nome VARCHAR(200) NOT NULL UNIQUE -- CONSTRAINT INLINE: regras na mesma linha
+,sigla CHAR(2) NOT NULL UNIQUE
+,ativo CHAR(1) NOT NULL DEFAULT 'S' -- Linha de Definição
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- ,CHECK (ativo IN ('S','N')) -- CONSTRAINT OUT OF LINE: regras definida fora da Linha de Definição SEM NOME
+-- ,CONSTRAINT pk_estado_id PRIMARY KEY (id)
+,CONSTRAINT regra_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S','N')) -- CONSTRAINT OUT OF LINE: regras definida fora da Linha de Definição COM NOME
+);
+
+/** EXEMPLO TABELA SEM REGRAS **/
+-- INSERT INTO estado(id,nome,sigla,ativo,data_cadastro) VALUES(1,'PARANÁ','PR','S','2020-12-15');
+-- INSERT INTO estado(id,nome,sigla,ativo,data_cadastro) VALUES(2,'PARANÁ','PR','S','2020-12-15');
+-- INSERT INTO estado(id,nome,sigla,ativo,data_cadastro) VALUES(3,'AMAZONAS','AM','S','2020-12-15');
+
+/** EXEMPLO TABELA COM REGRAS **/
+INSERT INTO estado(nome,sigla) VALUES('PARANÁ','PR');
+INSERT INTO estado(nome,sigla) VALUES('SÃO PAULO','SP');
+INSERT INTO estado(nome,sigla) VALUES('AMAZONAS','AM');
+
+SELECT id,nome,sigla,ativo,data_cadastro FROM estado;
+
+/** criando a tabela cidade **/
+CREATE TABLE cidade(
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR (200) NOT NULL
+,ativo CHAR(1) NOT NULL DEFAULT 'S' 
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,estado_id INT NOT NULL
+/** REGRAS DA TABELA **/
+,CONSTRAINT pk_cidade PRIMARY KEY (id)
+,CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+,CONSTRAINT somente_valor_S_ou_N CHECK (ativo IN('S', 'N'))
+,CONSTRAINT cidade_unica UNIQUE(nome, estado_id)
+);
+
+INSERT INTO cidade (nome,estado_id) VALUES ('CURITIBA', 1);
+
+SELECT * FROM cidade;
